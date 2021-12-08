4188abf0196c087c506a47f219cac6933d30015c
Sanjeev K. Sinha <sanksinh@in.ibm.com>
2021-12-08 16:48:43 +0530

    updated

--- modal.js
+++ modal.js
@@ -1,16 +1,27 @@
-
 // Get the modal
+var modal = document.getElementById("myBtiModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgbti = document.getElementById("myBtiImg");
+var modalImg = document.getElementById("imgbti01");
+var captionText = document.getElementById("caption");
+imgbti.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+var captionText = document.getElementById("captionbti");
+img.onclick = function () {
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
-}
+};
 
 var modal2 = document.getElementById("myModal2");
 
@@ -18,11 +29,11 @@ var modal2 = document.getElementById("myModal2");
 var img2 = document.getElementById("myImg2");
 var modalImg2 = document.getElementById("img02");
 var captionText2 = document.getElementById("caption2");
-img2.onclick = function(){
+img2.onclick = function () {
   modal2.style.display = "block";
   modalImg2.src = this.src;
   captionText2.innerHTML = this.alt;
-}
+};
 
 var modal3 = document.getElementById("myModal3");
 
@@ -30,25 +41,26 @@ var modal3 = document.getElementById("myModal3");
 var img3 = document.getElementById("myImg3");
 var modalImg3 = document.getElementById("img03");
 var captionText3 = document.getElementById("caption3");
-img3.onclick = function(){
+img3.onclick = function () {
   modal3.style.display = "block";
   modalImg3.src = this.src;
   captionText3.innerHTML = this.alt;
-}
+};
 // Get the <span> element that closes the modal
-var span1 = document.getElementById("close1")
-var span2 = document.getElementById("close2")
-var span3 = document.getElementById("close3")
+var span1 = document.getElementById("close1");
+var span2 = document.getElementById("close2");
+var span3 = document.getElementById("close3");
+var span4 = document.getElementById("closebti");
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function() { 
+span1.onclick = function () {
   modal.style.display = "none";
- 
-}
-span2.onclick = function() { 
-  
-    modal2.style.display = "none";
-  }
-  span3.onclick = function() { 
-  
-    modal3.style.display = "none";
-  }
+};
+span2.onclick = function () {
+  modal2.style.display = "none";
+};
+span3.onclick = function () {
+  modal3.style.display = "none";
+};
+span4.onclick = function () {
+  myBtiModal.style.display = "none";
+};
